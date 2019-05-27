53580439f67fad9071c6a34faedc8c2ca674fdce
jm-gutierrezh <jgutierrezh0@gmail.com>
2019-05-26 20:37:05 -0500

    First commit

---
+++ setup.py
@@ -0,0 +1,28 @@
+from distutils.core import setup
+
+
+def readme():
+    """Import the README.md Markdown file and try to convert it to RST format."""
+    try:
+        import pypandoc
+        return pypandoc.convert('README.md', 'rst')
+    except(IOError, ImportError):
+        with open('README.md') as readme_file:
+            return readme_file.read()
+
+
+setup(
+    name='titanic',
+    version='0.1',
+    description='Analysis of the Titanic dataset',
+    long_description=readme(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+    ],
+    url='https://github.com/jm-gutierrezh/titanic_datascience',
+    author='Juan Manuel Gutierrez',
+    author_email='jgutierrezh0@gmail.com',  # Substitute your email
+    license='MIT',
+    packages=['titanic'],
+    install_requires=['pypandoc>=1.4']
+)
